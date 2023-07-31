fdf713a8131bfd820189e7d562247f119c76fe17
khanhpham-nano <kpham@nanolos.com>
2023-08-01 00:47:06 +0700

    Each box should only contain 1 digit (0-9)

--- src/pin/PinInput.tsx
+++ src/pin/PinInput.tsx
@@ -14,8 +14,10 @@ const PinInput: React.FC<PinInputProps> = ({ pinCount }) => {
   }, [pinCount]);
 
   const handlePinChange = (index: number, value: string) => {
+    // Only allow digits 0-9 in the input
+    const sanitizedValue = value.replace(/[^0-9]/g, '');
     const newPins = [...pins];
-    newPins[index] = value;
+    newPins[index] = sanitizedValue.slice(0, 1); // Limit to one digit
     setPins(newPins);
 
     // Move focus to the next empty box, if available
@@ -32,8 +34,15 @@ const PinInput: React.FC<PinInputProps> = ({ pinCount }) => {
           key={index}
           ref={(input) => (inputRefs.current[index] = input!)}
           type="text"
+          pattern="[0-9]"
           value={pin}
           onChange={(e: ChangeEvent<HTMLInputElement>) => handlePinChange(index, e.target.value)}
+          onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+            // Allow navigating back to the previous box if the current box is empty and the user presses the backspace key
+            if (e.key === 'Backspace' && pin === '' && inputRefs.current[index - 1]) {
+              inputRefs.current[index - 1].focus();
+            }
+          }}
           size={1}
           maxLength={1}
         />
